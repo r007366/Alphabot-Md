7fff6a4944648ca9b14031ef83cbc3e1054f0f14
r007366 <36005766+r007366@users.noreply.github.com>
2023-01-12 00:43:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,24 +31,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "ℜ-Bot" //namabot kalian
+global.ownername = "ℜ𝔫𝔤𝔤𝔞" //nama kalian
+global.myweb = "https://s.id/frnknstn0" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@razor252" //bebas asal jan hapus
+global.myweb2 = "https://s.id/frnknstn0" //bebas
+global.email = "ranggaalfiansyah01@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6281380523725'] //premium user
+global.owner = ["6285591339086", "6287871677892"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285591339086" // nomor wa kalian
+global.ownernomerr = "+6287871677892" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'ℜ-Bot' //sticker wm ubah
+global.author = 'This Bot Made by Rangga' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
